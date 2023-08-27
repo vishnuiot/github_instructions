b86608d7996aea74b54b204d9ca59853e5ebdbef
vishnuiot <vishnuprasad.iit@gmail.com>
2023-08-27 17:16:26 +0200

    modification

--- gitupload.sh
+++ gitupload.sh
@@ -3,5 +3,5 @@ git commit -m "modification"
 git remote add origin git@github.com:vishnuiot/github_instructions
 # pull origin "main branch" or anyother branch
 # git pull origin myFeature
-# push origin "main branch"
+# push origin "main branch" or anyother branch
 git push origin myFeature
