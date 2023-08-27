2350480a14e6b924ee12c23e59a958017913827a
vishnuiot <vishnuprasad.iit@gmail.com>
2023-08-27 20:08:09 +0200

    modification

--- 05.mergebranchwithmain.sh
+++ 05.mergebranchwithmain.sh
@@ -12,3 +12,7 @@ git remote add origin git@github.com:vishnuiot/github_instructions
 
 #step 4 : Change contents to main in remote branch
 git push origin main
+
+#step 5: Perform this step to import contents from remotebranch to local branch
+# git pull origin myFeature
+
