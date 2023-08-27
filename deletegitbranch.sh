22eebc6e14b67206a8821d67b2b8c638b288fe3e
vishnuiot <vishnuprasad.iit@gmail.com>
2023-08-27 18:40:05 +0200

    delete branch

---
+++ deletegitbranch.sh
@@ -0,0 +1,27 @@
+git push origin --delete <branch>
+git branch --delete myFeature2
+#step 1 : change to main/master branch
+# git checkout main
+
+# #step 2 : create a  branch called myfeature
+# git checkout -b myFeature2 main
+# # git commit -am "Your message"
+
+# #step 3 : switch to created branch
+# git checkout myFeature2
+
+# #step 4 : Add new files after new files are added to the localfolder for upload into github
+# git add .
+# git commit -m "modification"
+# git remote add origin git@github.com:vishnuiot/github_instructions
+
+#step 5: Perform this step to import contents from remotebranch to local branch
+# git pull origin myFeature2
+
+#step 6: Push code into remoteserver-branch 
+# git push origin myFeature2
+
+# delete a file locally and update the remoteserver-branch
+# git push --force -u origin myFeature2
+
+# git push origin –d myFeature2
