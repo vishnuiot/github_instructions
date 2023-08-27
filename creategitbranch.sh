fc3d4596ff9ef85e130cad4e96eb31b905420e93
vishnuiot <vishnuprasad.iit@gmail.com>
2023-08-27 16:58:44 +0200

    modification

---
+++ creategitbranch.sh
@@ -0,0 +1,24 @@
+
+#step 1 : change to main/master branch
+git checkout main
+
+#step 2 : create a  branch called myfeature
+git checkout -b myFeature main
+# git commit -am "Your message"
+
+#step 3 : switch to created branch
+git checkout myFeature
+
+# Add new files to the branch locally for upload to github
+git add .
+git commit -m "modification"
+git remote add origin git@github.com:vishnuiot/testfolder
+git pull origin myFeature
+git push origin myFeature
+# #merge changes without fastforward
+# git checkout dev
+# git merge --no-ff myFeature
+
+# #now push changes to server
+# git push origin main
+# git push origin myFeature
